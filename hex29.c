1d5815171e3d220ede42963745ee6111ec5bbde0
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:52:07 +0300

    Add files via upload

---
+++ hex29.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      r       13.7
+float main(void) {
+    float a;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число радиан");
+    scanf_s("%f", &a);
+    printf("Площадь сектора = %4.3f\n", (r * r * a) / 2);
+    return 0;
+}
