f4bebc483beb768549e85930303c36f28f02b2f9
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 23:24:44 +0300

    Add files via upload

---
+++ ex32.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      D       2.54
+#define      K       2.32166
+int main()
+{
+	int dym;
+	float result, isp;
+	setlocale(LC_CTYPE, "RUS");
+	puts("Введите значение для расчета");
+	scanf_s("%d", &dym);
+	result = D * dym;
+	isp = K * dym;
+	printf("Перевод из дюймов: %2.2f см\n", result);
+	printf("Перевод из pulgada: %2.2f см", isp);
+}
