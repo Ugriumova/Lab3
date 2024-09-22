9de390ad654dda8ff194d6bb2b8c496ed81ddb1d
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:31:55 +0300

    Add files via upload

---
+++ ex20.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      bi       8
+#define      ba       1024
+float main() {
+    float p, p1;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите пропускную способность В бит/с");
+    scanf_s("%f", &p);
+    printf("Пропускная способность в Кбайт/с = %4.8f\n", p / (bi * ba));
+    puts("Введите пропускную способностьв Кбайт/с");
+    scanf_s("%f", &p1);
+    printf("Пропускная способность в бит/с = %4.8f", p1 * bi * ba);
+
+
+    return 0;
+}
