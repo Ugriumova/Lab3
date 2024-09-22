4dda1fee652d555e16538bfea9cf45a0d97f1a82
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:18:18 +0300

    Add files via upload

---
+++ ex12.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define      G       6.67*pow(10,-11)
+int main(void) {
+    float m1, m2, r, f;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите массу первого тела");
+    scanf_s("%f", &m1);
+    puts("Введите массу второго тела");
+    scanf_s("%f", &m2);
+    puts("Введите расстояние между телами");
+    scanf_s("%f", &r);
+    printf("Сила притяжения между телами = %4.10f",(G*m1 * m2) / r * r);
+    return 0;
+}
