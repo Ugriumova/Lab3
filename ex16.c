414af92926a39286c94ae07e6772f6af918dbd07
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:23:48 +0300

    Add files via upload

---
+++ ex16.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      m       12
+#define      d       365
+int main() {
+    float g;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число прожитых лет");
+    scanf_s("%f", &g);
+    printf("Число прожитых месяцев = %4.3f\n", g * m);
+    printf("Число прожитых дней = %4.3f\n", g * d);
+    return 0;
+}
