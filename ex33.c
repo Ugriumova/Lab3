6b94d856195389566f40c107bf333daa05fdcc65
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 20:35:40 +0300

    Add files via upload

---
+++ ex33.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+    int a, b;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите a и b");
+    scanf_s("%d%d", &a, &b);
+    puts(" _____________________________");
+    puts("|   a*b   |   a+b   |   a-b   |");
+    puts(" ------------------------------");
+    printf("| %3d*%3d | %3d+%3d | %3d-%3d |\n", a, b, a, b, a, b);
+    puts(" ------------------------------");
+    printf("| %7d | %7d | %7d |\n", a * b, a + b, a - b);
+    puts(" _____________________________");
+
+    return 0;
+}
