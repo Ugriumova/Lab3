c8ed163b7af2c6f6ed368524738a211223835805
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:57:47 +0300

    Add files via upload

---
+++ hex32.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      KK       238.845889
+#define      CH       0.277778
+float main(void) {
+    float mj;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число мегаджоулей");
+    scanf_s("%f", &mj);
+    printf("%f мегаджоулей равно %4.3f килокалорий\n", mj, KK * mj);
+    printf("%f мегаджоулей равно %4.3f киловат/час\n", mj, CH * mj);
+    return 0;
+}
