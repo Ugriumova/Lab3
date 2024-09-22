87cfd2d7c066169e0d37b93422f7351b8dde9ec2
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 21:19:47 +0300

    Add files via upload

---
+++ ex10.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+    int d, m, g, d1, m1, g1;
+    setlocale(LC_CTYPE, "RUS");
+    printf("Введите день рождения - ");
+    scanf_s("%d", &d);
+    printf("Введите месяц рождения - ");
+    scanf_s("%d", &m);
+    printf("Введите год рождения - ");
+    scanf_s("%d", &g);
+    printf("Введите настоящий день  - ");
+    scanf_s("%d", &d1);
+    printf("Введите настоящий месяц  - ");
+    scanf_s("%d", &m1);
+    printf("Введите настоящий год - ");
+    scanf_s("%d", &g1);
+    if (m < m1)  printf("Ваш возраст = %d лет \n", g1 - g);
+    if (m > m1)  printf("Ваш возраст = %d лет \n", g1 - g - 1);
+    if (m == m1 && d < d1)  printf("Ваш возраст = %d лет \n", g1 - g);
+    if (m == m1 && d > d1)  printf("Ваш возраст = %d лет \n", g1 - g - 1);
+    if (m == m1 && d == d1) printf("Ваш возраст = %d лет \n", g1 - g);
+
+
+    return 0;
+}
