e76b34c83c3ac488fc11289bf20172af453a453b
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 21:12:47 +0300

    Add files via upload

---
+++ ex7.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+float main() {
+    float s, b;
+    setlocale(LC_CTYPE, "RUS");
+    printf("Введите расстояние, которое хотите проехать - ");
+    scanf_s("%f", &s);
+    printf("Введите стоимость бензина за единицу расстояния - ");
+    scanf_s("%f", &b);
+    printf("Стоимость поездки =  %5.3f\n", s * b);
+
+
+
+    return 0;
+}
