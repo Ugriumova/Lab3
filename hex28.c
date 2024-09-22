51aeb353e5dc0d346d862b722ea7b7dc77df732a
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:50:14 +0300

    Add files via upload

---
+++ hex28.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+float main(void) {
+    float h, o1, o2, s, g, g2;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите высоту");
+    scanf_s("%f", &h);
+    puts("Введите меньшее основание");
+    scanf_s("%f", &o1); \
+        puts("Введите большее основание");
+    scanf_s("%f", &o2);
+    s = (o2 - o1) / 2;
+    g2 = (s * s) + (h * h);
+    g = sqrt(g2);
+    printf("Периметр трапеции = %4.3f\n", o1 + o2 + g + g);
+    printf("Площадь трапеции = %4.3f\n", h * ((o1 + o2) / 2));
+    return 0;
+}
