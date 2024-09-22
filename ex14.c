63441c992c41c3eeb91aae142eeadd94b4ad0cdb
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:20:17 +0300

    Add files via upload

---
+++ ex14.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      k       273.15
+int main(void) {
+    float t;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите температуру в градусах по Цельсию");
+    scanf_s("%f", &t);
+    printf("Температура в градусах по Кельвину = %4.2f\n", t + k);
+    printf("Температура в градусах по Фаренгейту = %4.2f", (1.8 * t) + 32);
+
+    return 0;
+}
