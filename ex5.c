caa1498d29afa3e31fd7663008b4f483e2ad65b9
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 21:08:30 +0300

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+float main() {
+    float u, r;
+    setlocale(LC_CTYPE, "RUS");
+    printf("Введите напряжение - ");
+    scanf_s("%f", &u);
+    printf("Введите сопротивление - ");
+    scanf_s("%f", &r);
+    printf("Сила тока =  %5.3f\n", u / r);
+
+
+    return 0;
+}
