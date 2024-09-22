d1ac588873adb325ccaa8b61e1a59dd0619555d5
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 21:17:06 +0300

    Add files via upload

---
+++ ex9.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+    float t;
+    setlocale(LC_CTYPE, "RUS");
+    printf("Введите временной интервал в секундах - ");
+    scanf_s("%f", &t);
+    printf("Временной интервал в часах = %5.3f \n", t / 3600);
+    printf("Временной интервал в минутах = %5.3f \n", t / 60);
+
+    return 0;
+}
