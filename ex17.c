563dbb7f55956bab6a00c0ea88aefcfa4c2b1a49
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:25:30 +0300

    Add files via upload

---
+++ ex17.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      pi       3.14
+#define      k        1.33333333
+int main() {
+    float d, r;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите диаметр шара");
+    scanf_s("%f", &d);
+    r = d / 2;
+    printf("Площадь поверхности шара = %4.3f\n", 4 * pi * r * r);
+    printf("Объем шара = %4.3f\n", k * pi * r * r * r);
+    return 0;
+}
