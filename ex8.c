b262afc81afa045c2b650f28f9431bb2c6b38372
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 21:14:42 +0300

    Add files via upload

---
+++ ex8.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+float main() {
+    float s, p;
+    setlocale(LC_CTYPE, "RUS");
+    printf("Введите сумму вклада - ");
+    scanf_s("%f", &s);
+    printf("Введите процентную ставку - ");
+    scanf_s("%f", &p);
+    printf("Доход  =  %5.3f\n", (s / 100) * p);
+
+
+
+    return 0;
+}
