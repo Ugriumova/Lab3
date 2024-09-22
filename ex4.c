8c92815b684fe06f8ddaa16814472d7dba28ef49
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 20:52:11 +0300

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      g       9.8
+float main() {
+    float m, h;
+    setlocale(LC_CTYPE, "RUS");
+    printf("Введите массу тела - ");
+    scanf_s("%f", &m);
+    printf("Введите высоту - ");
+    scanf_s("%f", &h);
+    printf("Сила тяжести =  %5.3f\n", m * g);
+    return 0;
+}
