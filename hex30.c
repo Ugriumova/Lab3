a3c1c526e121950768d3a2e5c8d7fa052764fff2
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:53:59 +0300

    Add files via upload

---
+++ hex30.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      f       2.2
+#define      u       35.27
+float main(void) {
+    float k;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число килограмм");
+    scanf_s("%f", &k);
+    printf("Число фунтов = %4.3f\n", k * f);
+    printf("Число унций = %4.3f\n", k * u);
+    return 0;
+}
