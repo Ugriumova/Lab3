4f9e0d43712dd7061ec00bdf63a312336277fe3e
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 20:53:17 +0300

    Add files via upload

---
+++ ex3.c
@@ -0,0 +1,12 @@
+﻿ #include <stdio.h>
+ #include <locale.h>
+float main() {
+    float a;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите сторону куба");
+    scanf_s("%f", &a);
+    printf("Объем куба =  %3.3f\n", a * a * a);
+    printf("Площадь боковой поверхности =  %3.3f\n", 4 * a * a);
+
+    return 0;
+}
