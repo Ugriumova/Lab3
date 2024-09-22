07c9079603654dc9043359904d9393d507d32671
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:21:55 +0300

    Add files via upload

---
+++ ex15.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      e       63241.126
+int main() {
+    float g;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число световых лет");
+    scanf_s("%f", &g);
+    printf("Число астрономических единиц = %4.3f\n", g * e);
+    return 0;
+}
