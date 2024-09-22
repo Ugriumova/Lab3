ba9081efe06e1fdfbd5fd21ec722f69c6dc5cb36
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:46:34 +0300

    Add files via upload

---
+++ hex27.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+float main(void) {
+    float k, s;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число жителей");
+    scanf_s("%f", &k);
+    puts("Введите площадь территории");
+    scanf_s("%f", &s);
+    printf("Плотность населения = %4.3f\n", k / s);
+    return 0;
+}
