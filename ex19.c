30eec3a8ec82bed2858680ace35992e5595bbb36
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:29:52 +0300

    Add files via upload

---
+++ ex19.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      F       0.453592
+float main() {
+    float f1, k;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число фунтов");
+    scanf_s("%f", &f1);
+    k = f1 * F;
+    printf("%f фунтов равен %f килограмм\n", f1, k);
+    printf("%f фунтов равен %4.6f тонн\n", f1, k / 1000);
+    printf("%f фунтов равен %4.3f грамм\n", f1, k * 1000);
+
+    return 0;
+}
