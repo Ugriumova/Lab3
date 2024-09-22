672567deb1947fd5eb2e5e97f0364190693f122c
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:41:55 +0300

    Add files via upload

---
+++ hex25.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+float main(void) {
+    float a;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите длину ребра");
+    scanf_s("%f", &a);
+    printf("Площадь тетраэдра = %4.3f\n", a * a * sqrt(3));
+    printf("Объем тетраэдра = %4.3f\n", (a * a * a * sqrt(2)) / 12);
+    return 0;
+}
