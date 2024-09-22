98d065a24a26dfa400bdc62e963a15c324f85b29
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:36:02 +0300

    Add files via upload

---
+++ hex22.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+float main() {
+    float k, s;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите кол-во товара");
+    scanf_s("%f", &k);
+    puts("Введите стоимость единицы товара");
+    scanf_s("%f", &s);
+    printf("Стоимость покупки = %4.3f\n", k * s);
+    return 0;
+}
