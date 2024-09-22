3463884b3c1213b2c8a939cb5ced4f16dc9c5e3f
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 22:55:53 +0300

    Add files via upload

---
+++ hex31.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      B       136.4
+#define      G       3.785411784
+float main(void) {
+    float b, g;
+    setlocale(LC_CTYPE, "RUS");
+    puts("Введите число баррелей");
+    scanf_s("%f", &b);
+    puts("Введите число галлонов");
+    scanf_s("%f", &g);
+    printf("%f баррелей равен %4.3f кг\n", b, b * B);
+    printf("%f галлонов равен %4.3f кг\n", g, g * G);
+    return 0;
+}
