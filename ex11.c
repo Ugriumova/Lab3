b57a373faa2ed3788d48a476953e425f9763ef83
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 21:22:03 +0300

    Add files via upload

---
+++ ex11.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      c       0.3
+int main() {
+    float v;
+    setlocale(LC_CTYPE, "RUS");
+    printf("Введите частоту сигнала(от 50 до 220 ГГц)\n");
+    scanf_s("%f", &v);
+    printf("Длина волны = %3.3f\n", c / v);
+
+    return 0;
+}
